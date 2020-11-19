6c88c29e646dd83d33397e62389127c1671e0a8e
chrisjgf <chrisjgf@gmail.com>
2020-11-19 12:58:43 +0100

    Refactor & tidy up code

--- src/components/pages/Govern/GovernDAO.tsx
+++ src/components/pages/Govern/GovernDAO.tsx
@@ -8,6 +8,27 @@ import LeftArrow from '../../icons/left-arrow.svg';
 import MTAIcon from '../../icons/tokens/MTA.svg';
 import EtherIcon from '../../icons/tokens/Ether.svg';
 import { getEtherscanLink, truncateAddress } from '../../../utils/strings';
+import { Tooltip } from '../../core/ReactTooltip';
+
+enum State {
+  DEFAULT,
+  MSTABLE,
+  COMMUNITY,
+  PROTOCOL,
+}
+
+type Item = {
+  title: string;
+  tooltip?: string;
+  address?: string;
+  accent?: string;
+};
+
+// replace with data
+type TokenProps = { amount?: BigNumber; image?: string; key?: string };
+//
+
+const { DEFAULT, MSTABLE, COMMUNITY, PROTOCOL } = State;
 
 const Container = styled.div`
   width: 100%;
@@ -35,7 +56,7 @@ const Header = styled.div`
     font-size: 1.5rem;
     line-height: 1em;
     font-weight: 600;
-    margin-right: 1rem;
+    margin-right: 0.5rem;
   }
 
   p {
@@ -82,13 +103,12 @@ const StyledRow = styled.button`
   cursor: pointer;
 
   &:focus,
-  :active {
+  :active,
+  :hover {
     outline: none;
-    box-shadow: 0 0 2px inset ${({ theme }) => theme.color.blue};
-  }
-
-  &:hover {
-    background: #ececec;
+    span {
+      border-bottom: 1px rgb(37, 39, 45) solid;
+    }
   }
 `;
 
@@ -145,26 +165,54 @@ const NavigateButton = styled.button`
   height: 1.5rem;
   margin-right: 0.5rem;
   border: none;
+  cursor: pointer;
 `;
 
-const DefaultRow: FC<{
-  title: string;
-  color: string;
-  index: number;
-  onClick: (i: number) => void;
-}> = ({ title, color, index, onClick }) => (
-  <StyledRow onClick={() => onClick(index)}>
-    <Circle color={color} />
-    {title}
-  </StyledRow>
-);
+const mapDataToState = (state: State): Item => {
+  switch (state) {
+    case MSTABLE:
+      return {
+        title: 'mStable',
+        tooltip: 'Manages all public MTA treasury.',
+        address: '0xb8541e73aa47a847fa39e803d19a3f9b1bbc5a6c',
+        accent: '#000',
+      };
+    case COMMUNITY:
+      return {
+        title: 'Community',
+        tooltip: 'Distributes grants for community building initiatives.',
+        address: '0xb8541e73aa47a847fa39e803d19a3f9b1bbc5a6c',
+        accent: '#4C4FA8',
+      };
+    case PROTOCOL:
+      return {
+        title: 'Protocol',
+        tooltip: 'Executes and manages changes to the core mStable protocol.',
+        address: '0xb8541e73aa47a847fa39e803d19a3f9b1bbc5a6c',
+        accent: '#CC1010',
+      };
+    default:
+      return { title: 'DAOs' };
+  }
+};
 
-type TokenRowProps = { amount?: BigNumber; image?: string };
+const StateSelector: FC<{
+  state: State;
+  onClick: (i: State) => void;
+}> = ({ state, onClick }) => {
+  const { title, accent } = mapDataToState(state);
+  return (
+    <StyledRow onClick={() => onClick(state)}>
+      <Circle color={accent ?? '#000'} />
+      <span>{title}</span>
+    </StyledRow>
+  );
+};
 
-const TokenRow: FC<TokenRowProps> = ({ image, amount }) => {
+const TokenItem: FC<TokenProps> = ({ image, amount, key }) => {
   const valueRounded = amount && Math.floor(amount.toNumber());
   return (
-    <StyledToken>
+    <StyledToken key={key}>
       <Circle color="#eee">
         <img src={image} alt="mta" />
       </Circle>
@@ -173,98 +221,54 @@ const TokenRow: FC<TokenRowProps> = ({ image, amount }) => {
   );
 };
 
-enum State {
-  DEFAULT = 'default',
-  MSTABLE = 'mstable',
-  COMMUNITY = 'community',
-  PROTOCOL = 'protocol',
-}
-
-const { DEFAULT, MSTABLE, COMMUNITY, PROTOCOL } = State;
-
-const mapTitleForState = (state: State): string => {
-  switch (state) {
-    case DEFAULT:
-      return 'DAOs';
-    case MSTABLE:
-      return 'mStable';
-    case COMMUNITY:
-      return 'Community';
-    case PROTOCOL:
-      return 'Protocol';
-    default:
-      return '';
-  }
-};
-
-const mapAddressForState = (state: State): string | undefined => {
-  switch (state) {
-    case MSTABLE:
-      return '0xb8541e73aa47a847fa39e803d19a3f9b1bbc5a6c';
-    case COMMUNITY:
-      return '0xb8541e73aa47a847fa39e803d19a3f9b1bbc5a6c';
-    case PROTOCOL:
-      return '0xb8541e73aa47a847fa39e803d19a3f9b1bbc5a6c';
-    default:
-      return undefined;
-  }
-};
-
-const itemColors = ['#000', '#4C4FA8', '#CC1010', '#4C4FA8', '#000'];
-
-const mockTokens: { image: string; amount: BigNumber }[] = [
+const mockTokens: TokenProps[] = [
   {
     image: MTAIcon,
     amount: new BigNumber(12010),
+    key: `token-0`,
   },
   {
     image: EtherIcon,
     amount: new BigNumber(1231),
+    key: `token-1`,
   },
   {
     image: MTAIcon,
     amount: new BigNumber(23043),
+    key: `token-2`,
   },
 ];
 
 export const GovernDAO: FC = () => {
-  const [state, setState] = useState<State>(State.DEFAULT);
-  const items = [
-    State.MSTABLE,
-    State.COMMUNITY,
-    State.PROTOCOL,
-    State.COMMUNITY,
-    State.PROTOCOL,
-  ];
-  const isDefault = state === State.DEFAULT;
+  const [state, setState] = useState<State>(DEFAULT);
 
-  const handleNavigationBack = useCallback(() => setState(State.DEFAULT), []);
+  const handleNavigateClick = useCallback(() => setState(DEFAULT), []);
+  const handleStateSelect = useCallback((s: State) => setState(s), []);
 
-  const handleDAOSelection = useCallback(
-    (i: number | undefined) => setState(items[i ?? -1] ?? State.DEFAULT),
-    [items],
-  );
-
-  const title = mapTitleForState(state);
-  const address = mapAddressForState(state);
+  const { title, address, tooltip } = mapDataToState(state);
 
   // mocked
-  const tokenMapping: Map<State, TokenRowProps[]> = new Map([
+  const tokenMapping: Map<State, TokenProps[]> = new Map([
     [MSTABLE, mockTokens],
     [COMMUNITY, mockTokens],
     [PROTOCOL, []],
-  ]); //
-
+  ]);
   const tokens = tokenMapping.get(state);
+  //
+
+  const items = [MSTABLE, COMMUNITY, PROTOCOL];
+  const hasTokens = tokens?.length !== 0;
+  const isDefault = state === DEFAULT;
 
   return (
     <Container>
       <Header>
         <div>
           {!isDefault && (
-            <NavigateButton type="button" onClick={handleNavigationBack} />
+            <NavigateButton type="button" onClick={handleNavigateClick} />
           )}
           <h3>{title}</h3>
+          {tooltip && <Tooltip tip={tooltip} />}
         </div>
         {address && (
           <Address
@@ -277,25 +281,17 @@ export const GovernDAO: FC = () => {
         )}
       </Header>
       <Items>
-        {isDefault &&
-          items.map((item, i) => (
-            <DefaultRow
-              index={i}
-              key={`${item}-select`}
-              title={mapTitleForState(item)}
-              color={itemColors[i]}
-              onClick={handleDAOSelection}
-            />
-          ))}
-        {!isDefault &&
-          (tokens?.length !== 0 ? (
-            tokens?.map(({ image, amount }, i) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <TokenRow image={image} amount={amount} key={`${i}-token`} />
-            ))
-          ) : (
-            <p>This DAO manages no funds.</p>
-          ))}
+        {isDefault ? (
+          items.map(s => (
+            <StateSelector state={s} onClick={handleStateSelect} />
+          ))
+        ) : hasTokens ? (
+          tokens?.map(({ image, amount, key }) => (
+            <TokenItem image={image} amount={amount} key={key} />
+          ))
+        ) : (
+          <p>This DAO currently manages no funds.</p>
+        )}
       </Items>
     </Container>
   );
